1c956679923a3cc5ece7524c2ef87a6fe8d1bc18
lixm1988 <527725681@qq.com>
2019-05-06 11:08:50 +0800

    修改登录时的界面切换顺序

--- app/views/login/form.js
+++ app/views/login/form.js
@@ -137,14 +137,15 @@ class LoginForm extends PureComponent {
 				logout();
 				return false;
 			}
-			//这个放到成功里
-			globalAction({
-				emclient: this.emclient
-			});
-			localStorage.setItem("userInfo", JSON.stringify(userInfo));
-			requestLogin(userInfo);
+			
 			this.props.reduxProps.history.push("/chats/recents");
 		});
+		//这个放到成功里
+		globalAction({
+			emclient: this.emclient
+		});
+		localStorage.setItem("userInfo", JSON.stringify(userInfo));
+		requestLogin(userInfo);
 		}
 		else{
 			setNotice("当前网络不可用，请检查网络状态", "fail");
